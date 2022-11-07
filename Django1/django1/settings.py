3b68c8ee1645ef8c9143e5681f31eba26fe31fa0
Igor <igor.torquato.20@gmail.com>
2022-11-07 19:08:05 -0300

    07/11/22

--- Django1/django1/settings.py
+++ Django1/django1/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@!&fkyen6+y$p^k+3pz4plsi753#smf%v=em2#8k_4f&!egt-x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 

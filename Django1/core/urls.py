803c9f4791c94317237ceec29df8d1215d2c9a0a
Igor <igor.torquato.20@gmail.com>
2022-11-12 11:27:36 -0300

    12/11/22

--- Django1/core/urls.py
+++ Django1/core/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import index, contato
 
 urlpatterns = [
-    path(' ',index),
+    path('',index), #raiz (endereço principal) obs: Aspas sem espaço
     path('contato',contato)
 ]
